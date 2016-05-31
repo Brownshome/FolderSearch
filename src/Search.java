d87100a4f1993dd118056eb19e5e89a72698e05a
James Brown <jamesphone180@gmail.com>
2016-05-31 17:17:53 +1200

    Fix 1

--- src/Search.java
+++ src/Search.java
@@ -3,7 +3,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Search {
 	public static void main(String[] args) throws IOException {
@@ -11,66 +13,111 @@ public class Search {
 			throw new IllegalArgumentException("Usage [tags] [searchdir]");
 
 		List<String> tags = Files.readAllLines(Paths.get(args[0]));
+		
+		//Home rolled suffix tree for the CL lookup.
 		SearchTree tree = new SearchTree(tags);
+		
+		//Data structure for EB tag name lookups.
+		Map<String, Integer> counts = new HashMap<>();
+		for(String tag : tags) {
+			counts.put(tag.trim().toUpperCase(), 0);
+		}
 
-		Files.newDirectoryStream(Paths.get(args[1])).forEach(path -> {
+		System.out.println(counts.size() + " tags to search for");
+		System.out.println("Reference, Path, File, Line, Tag, Paramater");
+
+		//for every file
+		Files.walk(Paths.get(args[1])).forEach(path -> {
+			int[] count = new int[] {0}; //pointer to a counter for the line number
+
 			try {
-				int[] count = new int[] {0, 0};
-				String[] name = new String[1];
+				if(!Files.isDirectory(path)) {
+					if(path.toString().endsWith(".EB")) {
+						String[] name = new String[1];
 
-				Files.lines(path).forEachOrdered(line -> {
-					if(line.startsWith("&N ")) {
-						name[0] = line.substring(3).trim();
-					} else{ 
-						if(!line.startsWith("{")) {
-							List<SearchTree> subTrees = new ArrayList<>();
-							line.codePoints().forEachOrdered(c -> {
-								for(int i = 0; i < subTrees.size(); i++) {
-									SearchTree t = subTrees.get(i);
+						Files.lines(path).forEachOrdered(line -> {
+							if(line.startsWith("&N"))
+								name[0] = line.substring(3).trim();
 
-									if(t != null) {
-										subTrees.set(i, t = t.getTree(c));
+							if(!line.startsWith("{") && line.contains("=")) {
+								String t = line.split("=", 2)[1].trim().toUpperCase();
+
+								if(t.contains(".")) {
+									t = t.split("[.]")[0];
+								}
+
+								if(t.contains("\\"))
+									t = t.split("\\\\")[1];
+
+								if(counts.containsKey(t)) {
+									String par = line.split("=", 2)[0].trim();
+									found(t, path, count[0], name[0], par);
+									counts.put(t, counts.get(t) + 1);
+								}
+							}
+
+							count[0]++;
+						});
+					} else {
+						Files.lines(path).forEachOrdered(line -> {
+							if(!line.startsWith("--")) {
+								List<SearchTree> subTrees = new ArrayList<>();
+								line.codePoints().forEachOrdered(c -> {
+									
+									//take every subtree and traverse one step lower
+									for(int i = 0; i < subTrees.size(); i++) {
+										SearchTree t = subTrees.get(i);
+
 										if(t != null) {
-											if(t.getWord() != null) {
-												found(t.getWord(), path, count[0], count[1] - t.getWord().length(), name[0]);
+											subTrees.set(i, t = t.getTree(c));
+											if(t != null) {
+												if(t.getWord() != null) {
+													found(t.getWord(), path, count[0], line);
+													counts.put(t.getWord(), counts.get(t.getWord()) + 1);
+												}
 											}
 										}
 									}
-								}
 
-								SearchTree subTree = tree.getTree(c);
-								if(subTree != null) {
-									int index = -1;
-									for(int i = 0; i < subTrees.size(); i++) {
-										if(subTrees.get(i) == null)
-											index = i;
+									//add new subtrees from root tree
+									SearchTree subTree = tree.getTree(c);
+									if(subTree != null) {
+										int index = -1;
+										for(int i = 0; i < subTrees.size(); i++) {
+											if(subTrees.get(i) == null)
+												index = i;
+										}
+
+										//try to conserver space by reusing indices
+										if(index != -1) {
+											subTrees.set(index, subTree);
+										} else {
+											subTrees.add(subTree);
+										}
+
+										if(subTree.getWord() != null) {
+											found(subTree.getWord(), path, count[0], line);
+											counts.put(subTree.getWord(), counts.get(subTree.getWord()) + 1);
+										}
 									}
-
-									if(index != -1) {
-										subTrees.set(index, subTree);
-									} else {
-										subTrees.add(subTree);
-									}
-
-									if(subTree.getWord() != null) {
-										found(subTree.getWord(), path, count[0], count[1] - subTree.getWord().length(), name[0]);
-									}
-								}
-
-								count[1]++;
-							});
-							count[1] = 0;
+								});
+							}
+							
 							count[0]++;
-						}
+						});
 					}
-				});
+				}
 			} catch (Exception e) {
-				throw new RuntimeException(e);
+				//no error handling to ignore binary data files
 			}
 		});
 	}
 
-	private static void found(String word, Path path, int line, int column, String pointName) {
-		System.out.printf("Found %s in %s (%d, %d) p:%s%n", word, path.getFileName(), line + 1, column + 2, pointName);
+	private static void found(String word, Path path, int line, String pointName, String paramaterName) {
+		System.out.printf("%s, %s, %s, %d, %s, %s%n", word, path.getParent(), path.getFileName(), line + 1, pointName, paramaterName);
+	}
+
+	private static void found(String word, Path path, int line, String string) {
+		System.out.printf("%s, %s, %s, %d, --, %s%n", word, path.getParent(), path.getFileName(), line + 1, string);
 	}
 }
--- src/SearchTree.java
+++ src/SearchTree.java
@@ -1,15 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchTree {	
 	//abcdefghijklmnopqrstuvwxyz01234566789 SYMBOL
-	static final int NUMBER_OF_CHARS = 'Z' - 'A' + '9' - '0' + 1;
+	static final int NUMBER_OF_CHARS = 'Z' - 'A' + '9' - '0' + 3;
 	static final int SYMBOL = NUMBER_OF_CHARS - 1;
 	
 	static int translate(int c) {
 		c = Character.toUpperCase(c);
 		
 		if(Character.isDigit(c))
-			return c - '0' + 'Z' - 'A';
+			return c - '0' + 'Z' - 'A' + 1;
 		
 		if(c <= 'Z' && c >= 'A')
 			return c - 'A';
@@ -24,9 +25,11 @@ public class SearchTree {
 		int index = translate(tag.codePointAt(layer));
 		SearchTree currentTree = options[index];
 		
+		//add new subtree
 		if(currentTree == null) 
 			options[index] = new SearchTree();
 		
+		//populate tree with word or new subtree
 		if(tag.length() > layer + 1)
 			options[index].addTag(tag, layer + 1);
 		else
@@ -35,7 +38,34 @@ public class SearchTree {
 	
 	@Override
 	public String toString() {
-		return word == null ? "empty" : word;
+		List<String> tags = getTags();
+		if(tags.isEmpty())
+			return "Empty";
+		
+		String s = "[";
+		for(int i = 0; i < tags.size() && i < 10; i++) {
+			s += (i == 0 ? "" : ", ") + tags.get(i);
+		}
+		s += "]";
+		
+		if(tags.size() > 10)
+			s += "(" + tags.size() + " more )";
+		
+		return s;
+	}
+	
+	//used for debug
+	public List<String> getTags() {
+		ArrayList<String> l = new ArrayList<>();
+		
+		if(getWord() != null) l.add(getWord());
+		
+		for(SearchTree tree : options) {
+			if(tree != null)
+				l.addAll(tree.getTags());
+		}
+		
+		return l;
 	}
 	
 	public void setWord(String word) {
@@ -46,7 +76,7 @@ public class SearchTree {
 	
 	public SearchTree(List<String> input) {
 		for(String tag : input)
-			addTag(tag, 0);
+			addTag(tag.trim(), 0);
 	}
 	
 	public String getWord() {
